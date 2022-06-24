0a428c25b04e951c75846665069f970e3c11c09b
eduarda-239 <105071413+eduarda-239@users.noreply.github.com>
2022-06-24 19:15:12 -0300

    Add files via upload

---
+++ Página1.js
@@ -0,0 +1,8 @@
+var botao = document.getElementById ('botao');
+
+botao.addEventListener('focus', () => {
+    botao.style.borderColor = #4A5f6a 
+});
+botao.addEventListener('blur', () =>{
+    botao.style.borderColor = "#ccc";
+});
